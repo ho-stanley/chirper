6b8459f33496ca73d155a45a6b35bb660cea9340
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-09-22 17:48:46 +0200

    feat: add CommentCard component

---
+++ components/CommentCard.tsx
@@ -0,0 +1,26 @@
+import { Card, Text } from '@nextui-org/react';
+import { format } from 'date-fns';
+import { Comment } from '../typings/comment';
+
+type CommentCardProps = {
+  comment: Comment;
+};
+
+export default function CommentCard({ comment }: CommentCardProps) {
+  const { authorName, body, createdAt } = comment;
+
+  return (
+    <Card variant="flat">
+      <Card.Header>
+        <Text b>{authorName}</Text>
+        <Text css={{ ml: 'auto' }}>
+          {format(new Date(createdAt), 'yyyy-MM-dd HH:mm:ss')}
+        </Text>
+      </Card.Header>
+      <Card.Divider />
+      <Card.Body>
+        <Text>{body}</Text>
+      </Card.Body>
+    </Card>
+  );
+}
