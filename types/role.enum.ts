a10232203ccfeadeecd075ab829be17185787d2f
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-09-24 11:35:59 +0200

    feat: add signIn http request method

---
+++ types/role.enum.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-shadow
+enum Role {
+  User = 'USER',
+  Admin = 'ADMIN',
+}
+
+export default Role;
--- utils/http/axios-http.ts
+++ utils/http/axios-http.ts
@@ -1,5 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
+import { Credentials } from '../../types/auth/credentials';
+import { SignInResponse } from '../../types/auth/signin-response';
+import { API_URL } from '../config';
 
 export const fetcher = <T>(url: string) =>
   axios.get<T>(url).then((res) => res.data);
+
+export const signIn = (data: Credentials) =>
+  axios
+    .post<SignInResponse>(`${API_URL}/auth/login`, data)
+    .then((res) => res.data);
