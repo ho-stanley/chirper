1196eca9147a02bf5c3db2822eda1299b5345c9d
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-09-24 11:43:43 +0200

    feat: add next-auth for jwt auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable comma-dangle */
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import type { NextPage } from 'next';
 import { ReactElement, ReactNode } from 'react';
 import { NextUIProvider } from '@nextui-org/react';
+import { SessionProvider } from 'next-auth/react';
 import Layout from '../components/Layout';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -21,10 +21,12 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return getLayout(
-    <NextUIProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </NextUIProvider>
+    <SessionProvider session={pageProps.session}>
+      <NextUIProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </NextUIProvider>
+    </SessionProvider>
   );
 }
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,67 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable camelcase */
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { signIn } from '../../../utils/http/axios-http';
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        // Sign in with provided credentials
+        if (credentials) {
+          return signIn({
+            username: credentials.username,
+            password: credentials.password,
+          })
+            .then(({ access_token, role, username, id }) => {
+              const user = {
+                accessToken: access_token,
+                username,
+                role,
+                id,
+              };
+
+              return user;
+            })
+            .catch(() => null);
+        }
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      // Persist data to the token right after signin
+      if (user) {
+        token.accessToken = user.accessToken;
+        token.username = user.username;
+        token.role = user.role;
+        token.id = user.id;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      // Send properties to the client
+      if (token) {
+        session.user = {
+          accessToken: token.accessToken,
+          username: token.username,
+          role: token.role,
+          id: token.id,
+        };
+      }
+
+      return session;
+    },
+  },
+  session: {
+    strategy: 'jwt',
+  },
+});
