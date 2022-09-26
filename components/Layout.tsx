7e6d300fc614f867c84c3f6c1609329f88dfe4bf
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-09-26 18:53:35 +0200

    fix: hamburger menu will now close when selecting an item

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,20 +1,51 @@
-import { Navbar, Text } from '@nextui-org/react';
+import { Navbar, Text, Link as UiLink } from '@nextui-org/react';
 import Link from 'next/link';
-import { ReactNode } from 'react';
+import { useRouter } from 'next/router';
+import { ReactNode, useEffect, useState } from 'react';
 import { FaKiwiBird } from 'react-icons/fa';
 import Box from './Box';
-import BurgerLink from './BurgerLink';
 import NavbarAvatar from './NavbarAvatar';
 
 type LayoutProps = {
   children: ReactNode;
 };
 
+const menuItems = [
+  { label: 'Home', href: '/', key: 'home' },
+  { label: 'New Post', href: '/', key: 'new_post' },
+  { label: 'My Posts', href: '/me/posts', key: 'my_posts' },
+  { label: 'Search', href: '/', key: 'search' },
+];
+
 export default function Layout({ children }: LayoutProps) {
+  const { pathname } = useRouter();
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+
+  /**
+   * NextUI navbar component does not support hiding the toggle menu
+   * after an item is selected. We need to use a custom solution in
+   * the meantime.
+   */
+  useEffect(() => {
+    // Enable scrolling again after closing Navbar.Collapse
+    document.body.style.overflow = '';
+    if (isSideMenuOpen) document.body.style.overflow = 'hidden';
+  }, [isSideMenuOpen]);
+
+  // Toggles side menu when we pass flag={true}
+  const handleSideMenu = (flag = false) => {
+    if (flag) setIsSideMenuOpen(!isSideMenuOpen);
+    if (isSideMenuOpen) setIsSideMenuOpen(false);
+  };
+
   return (
     <Box id="container">
       <Navbar isBordered isCompact>
-        <Navbar.Toggle showIn="sm" />
+        <Navbar.Toggle
+          showIn="sm"
+          isSelected={isSideMenuOpen}
+          onChange={() => handleSideMenu(true)}
+        />
         <Link href="/">
           <Navbar.Brand
             css={{
@@ -38,11 +69,11 @@ export default function Layout({ children }: LayoutProps) {
           </Navbar.Brand>
         </Link>
         <Navbar.Content hideIn="sm" enableCursorHighlight activeColor="primary">
-          <Link href="/">Home</Link>
-          <Link href="/">New Post</Link>
-          <Link href="/">My Posts</Link>
-          <Link href="/">Search</Link>
-          <Link href="/">About</Link>
+          {menuItems.map((item) => (
+            <Link href={item.href} key={item.key}>
+              {item.label}
+            </Link>
+          ))}
         </Navbar.Content>
         <Navbar.Content
           css={{
@@ -54,16 +85,19 @@ export default function Layout({ children }: LayoutProps) {
         >
           <NavbarAvatar />
         </Navbar.Content>
-        <Navbar.Collapse disableAnimation disableBlur>
-          <BurgerLink href="/" linkKey="home">
-            Home
-          </BurgerLink>
-          <BurgerLink href="/chirp" linkKey="new_post">
-            New post
-          </BurgerLink>
-          <Navbar.CollapseItem>My posts</Navbar.CollapseItem>
-          <Navbar.CollapseItem>Search</Navbar.CollapseItem>
-          <Navbar.CollapseItem>About</Navbar.CollapseItem>
+        <Navbar.Collapse disableAnimation disableBlur isOpen={isSideMenuOpen}>
+          {menuItems.map((item) => (
+            <Navbar.CollapseItem
+              key={item.key}
+              isActive={pathname === item.href}
+            >
+              <Link href={item.href} passHref>
+                <UiLink color="inherit" onClick={() => handleSideMenu(true)}>
+                  {item.label}
+                </UiLink>
+              </Link>
+            </Navbar.CollapseItem>
+          ))}
         </Navbar.Collapse>
       </Navbar>
       {children}
