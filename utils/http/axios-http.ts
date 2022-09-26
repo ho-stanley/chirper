4c67fbc6f54e007c97f67d127a02be347eac4936
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-09-26 16:26:13 +0200

    feat: add signup user POST request method

--- utils/http/axios-http.ts
+++ utils/http/axios-http.ts
@@ -2,12 +2,21 @@
 import axios from 'axios';
 import { Credentials } from '../../types/auth/credentials';
 import { SignInResponse } from '../../types/auth/signin-response';
+import { SignupData, SignupResponse } from '../../types/signup';
 import { API_URL } from '../config';
 
 export const fetcher = <T>(url: string) =>
   axios.get<T>(url).then((res) => res.data);
 
+/**
+ * POST requests do not use SWR because it doesn't support
+ * one-off request such as POST at the moment.
+ */
+
 export const signIn = (data: Credentials) =>
   axios
     .post<SignInResponse>(`${API_URL}/auth/login`, data)
     .then((res) => res.data);
+
+export const signupUser = (data: SignupData) =>
+  axios.post<SignupResponse>(`${API_URL}/users`, data).then((res) => res.data);
