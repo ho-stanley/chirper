00f89d5dfc0208d21e435a09ff898351884b8319
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-09-27 13:37:28 +0200

    feat: add my-posts page
    Shows posts created by the signed in user.

---
+++ pages/me/posts/index.tsx
@@ -0,0 +1,40 @@
+import { Container, Text } from '@nextui-org/react';
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
+import useSWR from 'swr';
+import ErrorIndicator from '../../../components/ErrorIndicator';
+import LoadingIndicator from '../../../components/LoadingIndicator';
+import PostCard from '../../../components/PostCard';
+import PostsContainer from '../../../components/PostsContainer';
+import { Post } from '../../../types/post';
+import { API_URL } from '../../../utils/config';
+import { fetcher } from '../../../utils/http/axios-http';
+import { NextPageWithLayout } from '../../_app';
+
+const MyPostsPage: NextPageWithLayout = () => {
+  const { data } = useSession();
+  const { data: posts, error } = useSWR<Post[]>(
+    data?.user ? `${API_URL}/posts?userId=${data.user.id}` : null,
+    fetcher
+  );
+
+  if (error) return <ErrorIndicator />;
+  if (!posts) return <LoadingIndicator />;
+
+  return (
+    <Container gap={1}>
+      <Text h2>My posts</Text>
+      <PostsContainer>
+        {posts.map((post) => (
+          <Link href={`/posts/${post.id}`} key={post.id}>
+            <div>
+              <PostCard post={post} isPressable />
+            </div>
+          </Link>
+        ))}
+      </PostsContainer>
+    </Container>
+  );
+};
+
+export default MyPostsPage;
