d3d7045dbb44f23a7894363c48b412b6dc9b24fd
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-09-27 13:54:53 +0200

    refactor: change my-posts route
    My posts route changed from /me/posts to /my-posts

--- pages/my-posts.tsx
+++ pages/my-posts.tsx
@@ -2,14 +2,14 @@ import { Container, Text } from '@nextui-org/react';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import useSWR from 'swr';
-import ErrorIndicator from '../../../components/ErrorIndicator';
-import LoadingIndicator from '../../../components/LoadingIndicator';
-import PostCard from '../../../components/PostCard';
-import PostsContainer from '../../../components/PostsContainer';
-import { Post } from '../../../types/post';
-import { API_URL } from '../../../utils/config';
-import { fetcher } from '../../../utils/http/axios-http';
-import { NextPageWithLayout } from '../../_app';
+import ErrorIndicator from '../components/ErrorIndicator';
+import LoadingIndicator from '../components/LoadingIndicator';
+import PostCard from '../components/PostCard';
+import PostsContainer from '../components/PostsContainer';
+import { Post } from '../types/post';
+import { API_URL } from '../utils/config';
+import { fetcher } from '../utils/http/axios-http';
+import { NextPageWithLayout } from './_app';
 
 const MyPostsPage: NextPageWithLayout = () => {
   const { data: session } = useSession();
