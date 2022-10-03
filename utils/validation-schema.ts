09e538702377288475f0e29d8a85d07880be5e66
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-10-03 10:59:32 +0200

    feat: add new user schema validation

--- utils/validation-schema.ts
+++ utils/validation-schema.ts
@@ -56,3 +56,32 @@ export const searchSchema = z.object({
     .max(50, { message: 'Search word must contain at most 50 characters' })
     .trim(),
 });
+
+export const newUserSchema = z
+  .object({
+    username: z
+      .string()
+      .regex(/^\w+$/, {
+        message: 'Only A-Z letters, numbers and underscore are allowed',
+      })
+      .min(3, { message: 'Username must contain at least 3 characters' })
+      .max(25, { message: 'Username must contain at most 25 characters' })
+      .trim(),
+    password: z
+      .string()
+      .min(8, { message: 'Password must contain at least 8 characters' })
+      .max(250, { message: 'Password must contain at most 250 characters' })
+      .trim(),
+    repeatPassword: z
+      .string()
+      .min(8, { message: 'Repeat password must contain at least 8 characers' })
+      .max(250, {
+        message: 'Repeat password must contain at most 250 characters',
+      })
+      .trim(),
+    role: z.enum(['ADMIN', 'USER']),
+  })
+  .refine((data) => data.password === data.repeatPassword, {
+    message: "Passwords don't match",
+    path: ['repeatPassword'],
+  });
