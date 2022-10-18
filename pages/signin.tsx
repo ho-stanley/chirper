62daeb4dea9d2b27af68699879a72268a1dbe8ed
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-10-18 18:07:39 +0200

    feat: add error handling and message in signin page

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -2,6 +2,8 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, Input, Spacer, Text } from '@nextui-org/react';
 import { signIn } from 'next-auth/react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import Form from '../components/Form';
 import FormContainer from '../components/FormContainer';
@@ -19,10 +21,31 @@ const SigninPage: NextPageWithLayout = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<SigninInputs>({ resolver: zodResolver(signinSchema) });
+  const [signingIn, setSigningIn] = useState(false);
+  const router = useRouter();
+  const [signInError, setSignInError] = useState('');
 
   const onSubmit: SubmitHandler<SigninInputs> = (data) => {
+    setSignInError('');
+    setSigningIn(true);
     const { username, password } = data;
-    signIn('credentials', { username, password, redirect: false });
+
+    signIn('credentials', { username, password, redirect: false })
+      .then((response) => {
+        if (response) {
+          if (response.ok) {
+            router.replace('/');
+            return;
+          }
+          if (response.status === 401) {
+            setSignInError('Invalid username or password');
+            return;
+          }
+          setSignInError('Sign in error');
+        }
+      })
+      .catch(() => setSignInError('Sign in error'))
+      .finally(() => setSigningIn(false));
   };
 
   return (
@@ -32,6 +55,11 @@ const SigninPage: NextPageWithLayout = () => {
       </Head>
       <FormContainer>
         <Text h2>Sign In</Text>
+        {signInError && (
+          <Text blockquote color="error">
+            {signInError}
+          </Text>
+        )}
         <Form onSubmit={handleSubmit(onSubmit)}>
           <Input
             required
@@ -59,7 +87,7 @@ const SigninPage: NextPageWithLayout = () => {
             <Text role="alert">{errors.password.message}</Text>
           )}
           <Spacer />
-          <Button auto size="lg" type="submit">
+          <Button auto size="lg" type="submit" disabled={signingIn}>
             Sign In
           </Button>
         </Form>
